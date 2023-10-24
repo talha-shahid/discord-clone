62ab79145c694c8d7dccdc576f3d7bae3c391546
Talha Shahid <talhashahidch@gmail.com>
2023-10-24 18:35:46 +0500

    Server Sidebar at 3 : 32

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
-// import { ServerSidebar } from "@/components/server/server-sidebar";
+import { ServerSidebar } from "@/components/server/server-sidebar";
 
 const ServerIdLayout = async ({
   children,
@@ -13,31 +13,31 @@ const ServerIdLayout = async ({
   children: React.ReactNode;
   params: { serverId: string };
 }) => {
-  //   const profile = await currentProfile();
+  const profile = await currentProfile();
 
-  //   if (!profile) {
-  //     return redirectToSignIn();
-  //   }
+  if (!profile) {
+    return redirectToSignIn();
+  }
 
-  //   const server = await db.server.findUnique({
-  //     where: {
-  //       id: params.serverId,
-  //       members: {
-  //         some: {
-  //           profileId: profile.id,
-  //         },
-  //       },
-  //     },
-  //   });
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
 
-  //   if (!server) {
-  //     return redirect("/");
-  //   }
+  if (!server) {
+    return redirect("/");
+  }
 
   return (
     <div className="h-full">
       <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        {/* <ServerSidebar serverId={params.serverId} /> */}
+        <ServerSidebar serverId={params.serverId} />
       </div>
       <main className="h-full md:pl-60">{children}</main>
     </div>
